34ef0744e49a01dd83ce5cc06c3c1edbefcc030d
Sebastian Górski <silent8668@gmail.com>
2019-08-13 16:38:02 +0200

    Implement search view functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,42 @@
 // Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
+/* ---Global state of the app---
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+    // 1. Get query form view
+    const query = searchView.getInput();
+
+    if (query) { // If there is a query
+        // 2. New search object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4. Search for recipes - we want to render result after we recaive data from API, so we await and do it as async
+       await state.search.getResults();
+
+        // 5. Render results on UI
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => { // e = event object
+    e.preventDefault(); // Prevent from reload
+    controlSearch();
+});
+
+
+
+
+

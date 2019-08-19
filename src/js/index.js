e229b5f592c7f3cfd54d00da18019fdae0c8132f
Sebastian Górski <silent8668@gmail.com>
2019-08-19 20:22:14 +0200

    Add uniqID package for shopping list elements, add List model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -113,5 +114,6 @@ elements.recipe.addEventListener('click', e => {
     } 
 });
 
+window.l = new List();
 
 

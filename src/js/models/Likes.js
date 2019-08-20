ca9421fcd7e8edbcf160f1e8df9ca3fea9e25b36
Sebastian Górski <silent8668@gmail.com>
2019-08-20 15:10:53 +0200

    Add localStorage save for likes menu

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -6,12 +6,19 @@ export default class Likes {
     addLike(id, title, author, img) {
         const like = { id, title, author, img };
         this.likes.push(like);
+
+        // Persist data in localStorage
+        this.persistData();
+
         return like;
     }
 
     deleteLike(id) {
         const index = this.likes.findIndex(el => el.id === id);
         this.likes.splice(index, 1);
+
+        // Persist data in localStorage
+        this.persistData();
     }
 
     isLiked(id) {
@@ -20,5 +27,16 @@ export default class Likes {
 
     getNumLikes() {
         return this.likes.length;
+    }
+
+    persistData() {
+        localStorage.setItem('likes', JSON.stringify(this.likes)); // JSON transform an array into a string
+    }
+
+    readStorage() {
+        const storage = JSON.parse(localStorage.getItem('likes')); // JSON transform a string into a data stuctures. If the storage is empty, this will return null, so we need to test that
+    
+        // Restore likes from the localStorage
+        if (storage) this.likes = storage;
     }
 }
